7744dd567d2844941040696e9849975e63260509
sanchez98 <ksanchez2715@educativacompartir.edu.co>
2024-11-16 12:22:12 -0500

    informacion.php

--- informacion.php
+++ informacion.php
@@ -1,10 +1,10 @@
 <?php
-$identificacion=$_post  ['identificacion'];
-$nomapellidos=$_post ['nomapellidos'];
+$Identificacion=$_post  ['Identificacion'];
+$Nomapellidos=$_post ['Nomapellidos'];
 $Cargo=$_post ['Cargo'];
 $Salario=$_post ['Salario'];
-echo "la identificacion del empleado es: ".identificacion;
-echo "Los nombres y apellidos del empleadoson: ".nomapellidos;
+echo "la identificacion del empleado es: ".Identificacion;
+echo "Los nombres y apellidos del empleadoson: ".Nomapellidos;
 echo "El cargo del empleado es: ".Cargo;
 echo "El salario del empelado es: ".Salario;
 ?>
